a48fc1d64d254f30956cede781be9ae6a6875d98
Luluu <146690389+NurulImaniar@users.noreply.github.com>
2025-03-07 22:28:24 +0800

    Add files via upload

---
+++ src/uji.java
@@ -0,0 +1,10 @@
+import javax.xml.crypto.Data;
+
+public class uji {
+    public static void main(String[] args) {
+        Database dt = new Database();
+        dt.open();
+        dt.view();
+
+    }
+}
